92e38a609117356b5d7dc079124825c291a2b5b7
LIMEUNBI <limeunbi@naver.com>
2025-12-26 12:46:04 +0900

    Build Setting, Structure Setting

--- app/src/main/java/com/eunbi/character/network/ApiServiceBuilder.kt
+++ app/src/main/java/com/eunbi/character/network/ApiServiceBuilder.kt
@@ -2,7 +2,6 @@ package com.eunbi.character.network
 
 class ApiServiceBuilder {
 
-
     suspend fun <T> apiGeneralBuilder(service: Class<T>): T {
         return ApiService.build(
             baseUrl = "https://rickandmortyapi.com/api/",
